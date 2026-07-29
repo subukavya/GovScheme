6ce7a1ed85105814b20988db957a1c414963b360
Kavya N <kavyasubu1207@gmail.com>
2026-07-29 15:22:42 +0000

    folder structure

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,30 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import Login from "./pages/Login";
+import Register from "./pages/Register";
+import LanguageSelection from "./pages/LanguageSelection";
+import ProfileMethod from "./pages/Profile";
+import ManualForm from "./pages/ManualForm";
+import OCRUpload from "./pages/OCRUpload";
+import VoiceInput from "./pages/VoiceInput";
+import Dashboard from "./pages/Dashboard";
+import NotFound from "./pages/NotFound";
+
 function App() {
   return (
-    <div className="min-h-screen bg-green-100 flex items-center justify-center">
-      <h1 className="text-5xl font-bold text-green-700">
-        GovScheme AI
-      </h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/language" element={<LanguageSelection />} />
+        <Route path="/profile-method" element={<ProfileMethod />} />
+        <Route path="/manual-form" element={<ManualForm />} />
+        <Route path="/ocr-upload" element={<OCRUpload />} />
+        <Route path="/voice-input" element={<VoiceInput />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- frontend/src/pages/ManualForm.tsx
+++
@@ -1,5 +0,0 @@
-function ManualForm() {
-  return <h1>Manual Form Page</h1>;
-}
-
-export default ManualForm;
